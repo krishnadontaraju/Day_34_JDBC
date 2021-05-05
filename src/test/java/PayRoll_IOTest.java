63660e7952929a9cc27e7a18263a7fdc242421f0
krishnadontaraju <reachkrishnadontaraju@gmail.com>
2021-05-05 17:20:42 +0530

    TEST : to validate database communication and retrieval

--- src/test/java/PayRoll_IOTest.java
+++ src/test/java/PayRoll_IOTest.java
@@ -32,5 +32,6 @@ public class PayRoll_IOTest {
         List<PayRoll>  dbPayRollData = dbPayRoll.readEmployeeDetails(PayRollOperation.IOService.DB_IO);
 
         Assertions.assertEquals(8,dbPayRollData.size());
+
     }
 }
